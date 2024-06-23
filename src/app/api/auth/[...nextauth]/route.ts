466897ffe7613491ad226443df4f465961f528da
sibin k <1sibinkeliyas123@gmail.com>
2024-06-23 12:52:55 +0530

    test 01-

--- src/app/api/auth/[...nextauth]/route.ts
+++ src/app/api/auth/[...nextauth]/route.ts
@@ -1,2 +1,31 @@
-import { handlers } from '@/auth';
-export const { GET, POST } = handlers;
+import CredentialProvider from 'next-auth/providers/credentials';
+import NextAuth, { NextAuthConfig } from 'next-auth';
+import prisma from '@/lib/neet.prisma';
+
+const authConfig: NextAuthConfig = {
+  session: {
+    strategy: 'jwt'
+  },
+  providers: [
+    CredentialProvider({
+      type: 'credentials',
+      credentials: {},
+      async authorize(credentials, req) {
+        const { email, password } = credentials as {
+          email: string;
+          password: string;
+        };
+        const user: any | null = await prisma.user.findFirst({
+          where: { email, password }
+        });
+        if (!user) throw new Error('Invalid credentials');
+        return user;
+      }
+    })
+  ],
+  pages : {
+    signIn : '/signin'
+  }
+};
+
+export default NextAuth(authConfig);
--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -5,6 +5,7 @@ import type { Metadata } from 'next';
 import NextTopLoader from 'nextjs-toploader';
 import { Inter } from 'next/font/google';
 import './globals.css';
+import { auth } from '@/auth';
 
 const inter = Inter({ subsets: ['latin'] });
 
@@ -18,11 +19,12 @@ export default async function RootLayout({
 }: {
   children: React.ReactNode;
 }) {
+  const session = await auth();
   return (
     <html lang="en" suppressHydrationWarning>
       <body className={`${inter.className} overflow-hidden`}>
         <NextTopLoader />
-        <Providers>
+        <Providers >
           <Toaster />
           {children}
         </Providers>
